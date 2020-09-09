69b1d8aba434e5351ee0392cd89b0ca5f724a9cd
syed <sabtain.info@gmail.com>
2020-09-09 11:58:32 -0700

    se prjoect

---
+++ src/Component/Cups/mugStore.js
@@ -0,0 +1,70 @@
+import image1 from '../../images/mugs/image1.jpg'
+import image2 from '../../images/mugs/image2.jpg'
+import image3 from '../../images/mugs/image3.jpg'
+import image4 from '../../images/mugs/image4.jpg'
+import image5 from '../../images/mugs/image5.jpg'
+import image6 from '../../images/mugs/image6.jpg'
+import image7 from '../../images/mugs/image7.jpg'
+import image8 from '../../images/mugs/image8.jpg'
+import image9 from '../../images/mugs/image9.jpg'
+import image10 from '../../images/mugs/image10.jpg'
+
+
+
+const mugstore ={
+
+    'CupsandMugs1': {
+        name:     'Mug',
+        img: image1,
+        price:600
+    },
+    'CupsandMugs2': {
+        name:     'Mug',
+        img: image2,
+        price:600
+    },
+    'CupsandMugs3': {
+        name:     'Mug',
+        img: image3,
+        price:600
+    },
+    'CupsandMugs4': {
+        name:     'Mug',
+        img: image4,
+        price:600
+    },
+    'CupsandMugs41': {
+        name:     'Mug',
+        img: image5,
+        price:600
+    },
+    'CupsandMugs5': {
+        name:     'Mug',
+        img: image6,
+        price:600
+    },
+    'CupsandMugs6': {
+        name:     'Mug',
+        img: image7,
+        price:600
+    },
+    'CupsandMugs7': {
+        name:     'Mug',
+        img: image8,
+        price:600
+    },
+    'CupsandMugs8': {
+        name:     'Mug',
+        img: image9,
+        price:600
+    },
+    'CupsandMugs9': {
+        name:     'Mug',
+        img: image10,
+        price:600
+    },
+
+
+}
+
+export default mugstore
---
+++ src/Component/Dinner/dinnerStore.js
@@ -0,0 +1,68 @@
+import image1 from '../../images/mugs/image1.jpg'
+import image2 from '../../images/mugs/image2.jpg'
+import image3 from '../../images/mugs/image3.jpg'
+import image4 from '../../images/mugs/image4.jpg'
+import image5 from '../../images/mugs/image5.jpg'
+import image6 from '../../images/mugs/image6.jpg'
+import image7 from '../../images/mugs/image7.jpg'
+import image8 from '../../images/mugs/image8.jpg'
+import image9 from '../../images/mugs/image9.jpg'
+import image10 from '../../images/mugs/image10.jpg'
+
+
+
+const mugstore ={
+
+    'Accessories': {
+        name:     'Accessories',
+        img: image1,
+        price:600
+    },
+    'Accessories': {
+        name:     'Accessories',
+        img: image2,
+        price:600
+    },
+    'Accessories': {
+        name:     'Accessories',
+        img: image3,
+        price:600
+    },
+    'Accessories': {
+        name:     'Accessories',
+        img: image4,
+        price:600
+    },
+    'Accessories': {
+        name:     'Accessories',
+        img: image5,
+        price:600
+    },
+    'Accessories': {
+        name:     'Accessories',
+        img: image6,
+        price:600
+    },
+    'Accessories': {
+        name:     'Accessories',
+        img: image7,
+        price:600
+    },
+    'Accessories': {
+        name:     'Accessories',
+        img: image8,
+        price:600
+    },
+    'Accessories': {
+        name:     'Accessories',
+        img: image9,
+        price:600
+    },
+    'Accessories': {
+        name:     'Accessories',
+        img: image10,
+        price:600
+    },
+
+
+}
